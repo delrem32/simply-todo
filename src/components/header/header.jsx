e2bf273cfdfeca107564cc6facabbc7c14988044
Andreas Aduschkin <delrem32@gmail.com>
2020-08-20 23:37:43 +0200

    buttons, header, logo

---
+++ src/components/header/header.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { connect } from "react-redux";
+import "./header.css";
+
+function Header() {
+	return (
+		<div className="wrapper">
+            <div className="start_items">
+                <button className="square_button">
+				    <i className="material-icons-outlined md-light md-20">apps_outlined</i>
+			    </button>
+			    <button className="square_button">
+				    <i className="material-icons-outlined md-light md-20">home_outlined</i>
+			    </button>
+			    <button className="square_button inline">
+				    <span className="material-icons-outlined md-light md-20">dashboard_outlined</span>
+			    </button>
+                <button className="square_button">
+                    <i className="material-icons-outlined md-light md-20">search_outlined</i>
+                </button>
+            </div>
+            <div className="logo" />
+            <div className="end_items">
+            <button className="square_button">
+				<i className="material-icons-outlined md-light md-20">add</i>
+			</button>
+			<button className="square_button">
+				<i className="material-icons-outlined md-light md-20">info_outlined</i>
+			</button>
+			<button className="square_button blau">
+				<i className="material-icons-outlined md-light md-20">notifications_outlined</i>
+			</button>
+            <button className="circle_button">
+				<i className="material-icons-outlined md-light md-20">account_circle</i>
+			</button>
+            </div>
+		</div>
+	);
+}
+
+export default connect(null, null)(Header);
