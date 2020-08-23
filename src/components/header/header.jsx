23f01864ede7c4dcf9d39b1ec344358c8ec581c4
Andreas Aduschkin <delrem32@gmail.com>
2020-08-23 12:46:01 +0200

    header menu, background

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,36 +4,58 @@ import "./header.css";
 
 function Header() {
 	return (
-		<div className="wrapper">
-            <div className="start_items">
-                <button className="square_button">
-				    <i className="material-icons-outlined md-light md-20">apps_outlined</i>
-			    </button>
-			    <button className="square_button">
-				    <i className="material-icons-outlined md-light md-20">home_outlined</i>
-			    </button>
-			    <button className="square_button inline">
-				    <span className="material-icons-outlined md-light md-20">dashboard_outlined</span>
-			    </button>
-                <button className="square_button">
-                    <i className="material-icons-outlined md-light md-20">search_outlined</i>
-                </button>
-            </div>
-            <div className="logo" />
-            <div className="end_items">
-            <button className="square_button">
-				<i className="material-icons-outlined md-light md-20">add</i>
-			</button>
-			<button className="square_button">
-				<i className="material-icons-outlined md-light md-20">info_outlined</i>
-			</button>
-			<button className="square_button blau">
-				<i className="material-icons-outlined md-light md-20">notifications_outlined</i>
-			</button>
-            <button className="circle_button">
-				<i className="material-icons-outlined md-light md-20">account_circle</i>
-			</button>
-            </div>
+		<div className="container">
+			<div className="header_background">
+				<span className="wrapper">
+					<div className="start_items">
+						<button className="square_button">
+							<i className="material-icons-outlined md-light md-20">
+								apps_outlined
+							</i>
+						</button>
+						<button className="square_button">
+							<i className="material-icons-outlined md-light md-20">
+								home_outlined
+							</i>
+						</button>
+						<button className="square_button inline">
+							<span className="material-icons-outlined md-light md-20">
+								dashboard_outlined
+							</span>
+						</button>
+						<button className="square_button">
+							<i className="material-icons-outlined md-light md-20">
+								search_outlined
+							</i>
+						</button>
+					</div>
+					<div className="end_items">
+						<button className="square_button">
+							<i className="material-icons-outlined md-light md-20">
+								add
+							</i>
+						</button>
+						<button className="square_button">
+							<i className="material-icons-outlined md-light md-20">
+								info_outlined
+							</i>
+						</button>
+						<button className="square_button blau">
+							<i className="material-icons-outlined md-light md-20">
+								notifications_outlined
+							</i>
+						</button>
+						<button className="circle_button">
+							<i className="material-icons-outlined md-light md-20">
+								account_circle
+							</i>
+						</button>
+					</div>
+				</span>
+			</div>
+			<div className="logo_wrapper">
+				<span className="logo"></span>
+			</div>
 		</div>
 	);
 }
